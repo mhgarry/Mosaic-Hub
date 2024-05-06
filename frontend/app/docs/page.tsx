667f5f2860d4d89f41da38ba3565f7b3116b7532
mhgarry <mhgarry92@gmail.com>
2024-05-05 22:13:47 -0400

    add button from nextui as test

--- frontend/app/docs/page.tsx
+++ frontend/app/docs/page.tsx
@@ -1,9 +1,15 @@
 import { title } from "@/components/primitives";
+import { Button } from "@nextui-org/react";
 
 export default function DocsPage() {
-	return (
-		<div>
-			<h1 className={title()}>Docs</h1>
-		</div>
-	);
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div>
+        <h1 className={title()}>Docs</h1>
+      </div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </section>
+  );
 }
